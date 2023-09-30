c8c68cbeac79c958976d59e5c25efdf937ad12df
furrysweet-bigmuff <greyhotel2@gmail.com>
2023-09-30 21:13:39 +0200

    t
    :wq

--- src/app/services/chatgpt.service.ts
+++ src/app/services/chatgpt.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { CHAT_TOKEN } from 'src/environmental';
 
 @Injectable({
   providedIn: 'root'
